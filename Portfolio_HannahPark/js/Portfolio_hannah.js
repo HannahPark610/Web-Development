badc05ff13f7541beee257a6a71a536529434e2c
Hannah Park <teyang610@gmail.com>
2016-12-15 12:47:26 -0800

    Something happened_Modify portfolio

---
+++ Portfolio_HannahPark/js/Portfolio_hannah.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  $("#thumbnail1").click(function(){
+    $("#group_thumbnails").fadeOut("slow", function(){
+      $("#content1").fadeIn("slow");
+    });
+  });
+
+$(function(){
+
+  $("#thumbnail2").click(function(){
+    $("#group_thumbnails").fadeOut("slow", function(){
+      $("#content2").fadeIn("slow");
+    });
+  });
+
+$(function(){
+
+  $("#thumbnail3").click(function(){
+    $("#group_thumbnails").fadeOut("slow", function(){
+      $("#content3").fadeIn("slow");
+    });
+  });
+
+$(function(){
+
+  $("#thumbnail4").click(function(){
+    $("#group_thumbnails").fadeOut("slow", function(){
+      $("#content4").fadeIn("slow");
+    });
+  });
+
+   });
+  });
+ });
+});
