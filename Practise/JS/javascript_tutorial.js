94f55796c710663673833f98122610e055170688
Hannah Park <teyang610@gmail.com>
2017-01-05 00:57:40 -0800

    study_javascript

--- Practise/JS/javascript_tutorial.js
+++ Practise/JS/javascript_tutorial.js
@@ -1,9 +1,16 @@
-var myString = undefined;
-var myNumber = undefined;
-var myBoolean = undefined;
+var myString = 'Hannah';
+var myNumber = 32;
+var myBoolean = true;
+
+console.log('365'/'27');
+//answer: 13.518518518518519
+console.log('365'%'27');
+//ansewer: 14
 
 
 // Do not edit the code under this line
 console.log("Name: " + myString);
 console.log("Lucky Number: " + myNumber);
 console.log("Good joke? " + myBoolean);
+
+console.log(Math.floor(Math.random()*100));
